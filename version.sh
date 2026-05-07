9523f849d046dec897640ce1500bd91d49ae16ba
MalashinG <118243807+MalashinG@users.noreply.github.com>
2026-05-07 18:18:54 +0300

    Clean up version.sh by removing comments and echoes
    Removed commented code and echo statements for clarity.

--- version.sh
+++ version.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Определение списка версий
-#versions=("postgresql15st-server" "postgresql16-server" "postgresql17-server" "postgresql18-server")
 declare -A services
 services["postgresql15st-server"]="postgresql15"
 services["postgresql16-server"]="postgresql16"
@@ -9,8 +7,6 @@ services["postgresql17-server"]="postgresql17"
 services["postgresql18-server"]="postgresql18"
 # Перебор массива
 for version in "${!services[@]}"; do
-    #echo "Обработка версии: $version"
-    
     sudo dnf install $version -y
     if [ $? -ne 0 ]; then
     echo "Ошибка!"
